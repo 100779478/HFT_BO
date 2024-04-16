e6742e4c8eab746a3f3571a294864445ae5383a8
acky <100779478@qq.com>
2024-04-16 15:37:27 +0800

    优化打包体积和构建速度

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const {defineConfig} = require('@vue/cli-service')
+const path = require('path');
+const {defineConfig} = require('@vue/cli-service');
 const TerserPlugin = require('terser-webpack-plugin');
+const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
 module.exports = defineConfig({
+    productionSourceMap: false,
     transpileDependencies: true,
     lintOnSave: false,
     configureWebpack: {
@@ -8,23 +11,76 @@ module.exports = defineConfig({
             minimizer: [
                 new TerserPlugin({
                     terserOptions: {
-                        ecma: 6,
+                        ecma: 2016, // 使用ES6语法
                         compress: {
-                            drop_console: true,
-                            drop_debugger: true,
+                            modules: true, // 开启模块压缩
                         },
                         output: {
-                            comments: false,
+                            comments: false, // 去除注释
                         },
                     },
                 }),
             ],
+            splitChunks: {
+                chunks: 'async', // 默认只分割异步引入代码块,可配置为  all
+                // ......此处省略一些
+                cacheGroups: { // splitChunks就是根据cacheGroups去拆分模块的
+                    vendors: {
+                        // 表示只筛选从node_modules文件夹下引入的模块，所以node_modules下的第三方模块才会被拆分出来。
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10 // 权重设置
+                    },
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true
+                    }
+                }
+            }
         },
+        plugins: []
     },
-    // devServer: {
-    //     headers: {
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    //     },
-    // },
+    chainWebpack: config => {
+        config.plugin('speed-measure-webpack-plugin')
+            .use(SpeedMeasureWebpackPlugin);
+
+        config.module.rule('vue')
+            .use('thread-loader')
+            .loader('thread-loader')
+            .before('thread-loader');
+
+        config.module.rule('js')
+            .test(/\.jsx?$/)
+            .exclude
+            .add(file => (
+                /node_modules/.test(file) &&
+                !/\.vue\.js/.test(file)
+            ))
+            .end()
+            .use('babel-loader')
+            .loader('babel-loader')
+            .options({
+                cacheDirectory: true
+            });
+
+        config.optimization.splitChunks({
+            chunks: 'all',
+            cacheGroups: {
+                libs: {
+                    name: 'chunk-libs',
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: 10,
+                    chunks: 'initial'
+                },
+            }
+        })
+
+    },
+// devServer: {
+//     headers: {
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+//     },
+// },
 })
+;
