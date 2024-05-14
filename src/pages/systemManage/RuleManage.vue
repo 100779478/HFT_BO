ca02db855c87d9e7d2d237ac5932b9a4d4e27bac
acky <100779478@qq.com>
2024-05-14 16:47:54 +0800

    编辑用户策略时切换策略类型导致路径置空bug修复

--- src/pages/systemManage/RuleManage.vue
+++ src/pages/systemManage/RuleManage.vue
@@ -626,7 +626,7 @@ export default {
           this.userStrategyInfo.rulePath = './Rules/libmm_strategy_fi.so'
           break
         default:
-          this.userStrategyInfo.rulePath = this.rulePath
+          this.userStrategyInfo.rulePath = this.rulePath || this.userStrategyInfo.rulePath
           break
       }
     }
